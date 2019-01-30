7b4114682e3f3a8cd0cbb3e8b828832a373458ef
declanmcgee01 <declanmcgee1995@gmail.com>
2019-01-30 17:39:19 +0000

    created user entity

---
+++ src/main/java/com/qa/persistence/domain/User.java
@@ -0,0 +1,45 @@
+package com.qa.persistence.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	
+	private Long userID;
+	private String username;
+	private String password;
+	
+	public User() {
+		
+	}
+
+	public Long getUserID() {
+		return userID;
+	}
+
+	public void setUserID(Long userID) {
+		this.userID = userID;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
