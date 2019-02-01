50842832cf71078f48d350077f22bb911c22f184
declanmcgee01 <declanmcgee1995@gmail.com>
2019-02-01 10:35:18 +0000

    dummy data

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO GOD (godID, name, godOf, children, powers) VALUES (1, 'Odin', 'Wisdom', 'Thor|Baldur', 'Strong')
-INSERT INTO GOD (godID, name, godOf, children, powers) VALUES (2, 'Thor', 'Thunder', 'Magni|Modi', 'Lightning')
-INSERT INTO GOD (godID, name, godOf, children, powers) VALUES (3, 'Baldur', 'Light', 'Forseti', 'Feels Nothing')
-
+INSERT INTO GOD (godID, name, godOf, children, powers, godImage) VALUES (1, 'Odin', 'Wisdom', 'Thor|Baldur', 'Strong', 'Odin.jpg')
+INSERT INTO GOD (godID, name, godOf, children, powers, godImage) VALUES (2, 'Thor', 'Thunder', 'Magni|Modi', 'Lightning', 'Thor.jpg')
+INSERT INTO GOD (godID, name, godOf, children, powers, godImage) VALUES (3, 'Baldur', 'Light', 'Forseti', 'Feels Nothing', 'Baldur.jpg')
+INSERT INTO USER (userID, username, godType) VALUES (1, 'Declan', 'Aesir')
 
