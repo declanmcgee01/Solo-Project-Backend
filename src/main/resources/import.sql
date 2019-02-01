2af16cafa323a78de9f65148974b5020decb14da
declanmcgee01 <declanmcgee1995@gmail.com>
2019-02-01 10:26:18 +0000

    create user finished

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
 INSERT INTO GOD (godID, name, godOf, children, powers, godImage) VALUES (1, 'Odin', 'Wisdom', 'Thor|Baldur', 'Strong', 'Odin.jpg')
 INSERT INTO GOD (godID, name, godOf, children, powers, godImage) VALUES (2, 'Thor', 'Thunder', 'Magni|Modi', 'Lightning', 'Thor.jpg')
 INSERT INTO GOD (godID, name, godOf, children, powers, godImage) VALUES (3, 'Baldur', 'Light', 'Forseti', 'Feels Nothing', 'Baldur.jpg')
-INSERT INTO USER (userID, username, password) VALUES (1, 'Declan' , 'password')
+INSERT INTO USER (userID, username, godType) VALUES (1, 'Declan' , 'Vanir')
 
