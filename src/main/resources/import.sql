2eaabdfcf9d764b18de805f69ff3a0bc7773bc17
declanmcgee01 <declanmcgee1995@gmail.com>
2019-02-08 11:04:39 +0000

    small fix

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,4 @@
-INSERT INTO USER (userID, username) VALUES (1, 'Declan')
+INSERT INTO USER (userID, username, password) VALUES (1, 'Declan', 'password')
 INSERT INTO GOD (godID, name, godOf, children, powers, godImage, userID) VALUES (1, 'Odin', 'Wisdom', 'Thor|Baldur', 'Strong', 'Odin.jpg', 1)
 INSERT INTO GOD (godID, name, godOf, children, powers, godImage, userID) VALUES (2, 'Thor', 'Thunder', 'Magni|Modi', 'Lightning', 'Thor.jpg', 1)
 INSERT INTO GOD (godID, name, godOf, children, powers, godImage, userID) VALUES (3, 'Baldur', 'Light', 'Forseti', 'Feels Nothing', 'Baldur.jpg', 1)
